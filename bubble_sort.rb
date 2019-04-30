8f114b5cc450242a383288587dd20c09b4415800
Ben Daniels <benbanj@hotmail.com>
2019-04-30 19:28:59 +0100

    Adding bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,3 +18,25 @@ def bubble_sort(arr)
 end
 
 p bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(arr)
+  loop do
+    swapped = false
+    i = 0
+    while i < (arr.size) -1
+      item1 = arr[i]
+      item2 = arr[i+1]
+      res = yield(item1, item2)
+      if res > 0
+        arr[i] = item2
+        arr[i+1] = item1
+        swapped = true
+      end
+      i += 1
+    end
+    break if !swapped
+  end
+  arr
+end
+
+p bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
